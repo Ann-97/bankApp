39b810393a50c43879af3a96191492ffde43d985
Ann <annmariavithayathil@gmail.com>
2022-04-01 22:13:37 +0530

    one way binding

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  aim="Your perfect banking partner"
+  accnum="Account number please!!"
+
+ database={
+   1000:{acno:1000,uname:"Neer",password:1000,balance:5000},
+   1001:{acno:1000,uname:"Niha",password:1000,balance:3000},
+   1002:{acno:1000,uname:"Neha",password:1000,balance:2000},
+ }
   constructor() { }
 
   ngOnInit(): void {
+  }
+  acnoChange(event:any){
+
+  }
+  login(){
+    alert("login clicked")
   }
 
 }
