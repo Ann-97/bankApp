03e8b29da63b2fd159dc1037b7b935e2befad5ca
Ann <annmariavithayathil@gmail.com>
2022-04-05 11:44:23 +0530

    event binding usind

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,21 +9,45 @@ export class LoginComponent implements OnInit {
 
   aim="Your perfect banking partner"
   accnum="Account number please!!"
+  acno=""
+  pswd=""
 
- database={
+ database:any ={
    1000:{acno:1000,uname:"Neer",password:1000,balance:5000},
-   1001:{acno:1000,uname:"Niha",password:1000,balance:3000},
-   1002:{acno:1000,uname:"Neha",password:1000,balance:2000},
+   1001:{acno:1001,uname:"Niha",password:1001,balance:3000},
+   1002:{acno:1002,uname:"Neha",password:1002,balance:2000},
  }
   constructor() { }
 
   ngOnInit(): void {
   }
   acnoChange(event:any){
-
+    this.acno=event.target.value
+    
   }
+
+  pswdChange(event:any){
+    this.pswd=event.target.value
+  }
+
   login(){
-    alert("login clicked")
-  }
+   var acno=this.acno;
+   var pswd=this.pswd;
 
+    let database=this.database
+
+   if(acno in database){
+     if(pswd == database[acno]["password"]){
+       alert("login successfull")
+     }
+     else{
+      alert("Incorrect password")
+   }
+  
+   }
+  
+   else{
+     alert("user doesnot exist")
+   }
+  }
 }
